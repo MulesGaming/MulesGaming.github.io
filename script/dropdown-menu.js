c249e14e5d5424d0616fa617344daa7a54e3e3e2
Mules Gaming <battledroidmule@gmail.com>
2023-09-14 17:05:29 -0500

    Added background to menu and Mastodon

--- script/dropdown-menu.js
+++ script/dropdown-menu.js
@@ -7,15 +7,24 @@ console.log("Dropdown menu.js loaded!");
 function dropdownMenuVisible (visibility) {
   const dropdownMenu = document.getElementById("dropdownMenuMain");
   const dropdownMenuButton = document.getElementById("dropdownIcon");
+  const dropdownBackground = document.getElementById("dropdownBackground");
   const main = document.querySelector("main");
   if (visibility === "open") {
     dropdownMenu.style.visibility = "visible";
+    dropdownBackground.style.visibility = "visible";
     dropdownMenuButton.innerHTML = null;
   }else if (visibility === "close") {
     dropdownMenu.style.visibility = "hidden";
+    dropdownBackground.style.visibility = "hidden";
     dropdownMenuButton.innerHTML = '<img class="dropdown-icon-image" src="/media/images/hamburger-menu-icon.svg">';
   }else{
     console.error("Given visibility is not open or close.");
   }
 }
+
+// Dropdown Backgroud close
+
+dropdownBackground.addEventListener('click', function() {
+  dropdownMenuVisible ('close')
+});
 
