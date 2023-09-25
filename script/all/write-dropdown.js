286b9b557ccf92cec91d56e9c69d1fc6f3effc28
Mules Gaming <110064958+MulesGaming@users.noreply.github.com>
2023-09-24 20:35:07 -0500

    Added more redirects

--- script/all/write-dropdown.js
+++ script/all/write-dropdown.js
@@ -20,7 +20,7 @@ const dropdownMenuMainItems = `
   <a href="/"><div class="dropdown-menu-item"><img class="dropdown-menu-img" src="/media/images/home-icon.svg" width="50">Home</div></a>
   <a href="/projects/"><div class="dropdown-menu-item"><img class="dropdown-menu-img" src="/media/images/wrench-icon.svg" width="50">Projects</div></a>
   <a href="/yt.html"><div class="dropdown-menu-item"><img class="dropdown-menu-img" src="/media/images/yt_icon_mono_dark.png" width="50">YouTube</div></a>
-  <a href="https://twitter.com/MulesGaming"><div class="dropdown-menu-item"><img class="dropdown-menu-img" src="/media/images/twitter_logo.png" width="50">Twitter</div></a>
+  <a href="/twt"><div class="dropdown-menu-item"><img class="dropdown-menu-img" src="/media/images/twitter_logo.png" width="50">Twitter</div></a>
   <a href="https://mastodon.social/@mulesgaming"><div class="dropdown-menu-item"><img class="dropdown-menu-img" src="/media/images/mastodon_logo.svg" width="50">Mastodon</div></a>
   <a href="https://github.com/MulesGaming"><div class="dropdown-menu-item"><img class="dropdown-menu-img" src="/media/images/github-logo-white.svg" width="50">GitHub</div></a>
 </div>
