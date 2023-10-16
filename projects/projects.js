11000c29ef93596572ad1f16e684c55a49cbff0f
Mules Gaming <110064958+MulesGaming@users.noreply.github.com>
2023-10-16 10:04:49 -0500

    reworded word

--- projects/projects.js
+++ projects/projects.js
@@ -3,7 +3,7 @@
 projects = [
   {name:"Minecraft Wordle",desc:"The Wordle, but Minecraft!",link:"https://minecraftwordle.fun",img:"https://minecraftwordle.fun/media/img/logo.png"},
   {name:"The Mystery of the 9 Wands",desc:"A Minecraft adventure map",link:"/projects/mystery-9-wands/",img:"https://camo.githubusercontent.com/facef823ffb2e8d96c8c7e7fc91f247fbf2dd6c13aab410a83df6fbe8dbd454c/68747470733a2f2f7777772e6d756c657367616d696e672e636f6d2f6d656469612f696d616765732f395f77616e64735f69636f6e2e706e67"},
-  {name:"My Minecraft skins",desc:"My skins for Minecraft!",link:"/projects/skins",img:""},
+  {name:"My Minecraft skins",desc:"My Minecraft skins!",link:"/projects/skins",img:""},
   {name:"My Datapacks for Minecraft",desc:"Datapacks I've made!",link:"/projects/datapacks",img:""},
   {name:"Search for videos",desc:"Search for videos across my channel!",link:"/projects/search",img:""}
 ];
