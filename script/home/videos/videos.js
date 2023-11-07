48589362b279ae4fa3419eed1686f69a1f263f1b
Mules Gaming <110064958+MulesGaming@users.noreply.github.com>
2023-11-06 20:35:08 -0600

    Cleaned up Youtube fetch

--- script/home/videos/videos.js
+++ script/home/videos/videos.js
@@ -2,6 +2,44 @@
 
 console.log("Videos.js loaded. This website uses YouTube API.")
 
+// Define functions
+
+function writeHTML(videoData){
+  let stats = videoData.items[0].statistics
+  let videos = videoData.items[0].snippet
+  let videoIndividualID = videoData.items[0].id
+  let videoContaner = document.querySelector(".videos-container")
+  // HTML
+  videoContaner.innerHTML += `
+  <section class="video">
+    <a class="videos-link" href="https://www.youtube.com/watch?v=${videoIndividualID}">
+      <img src="https://i.ytimg.com/vi/${videoIndividualID}/hq720.jpg" class="video-thumbnail" title="Thumbnail of ${videos.title}" alt="${videos.title}'s thumbnail">  
+      <h3 class="video-title" title="${videos.title}">${videos.title}</h3>
+      <div class="video-stats">
+        <img class="view-icon" src="/media/images/view-icon.svg" alt="A eye" title="views">
+        <p class="view-count">${stats.viewCount}</p>
+      </div>
+    </a>
+  </section>
+  `
+}
+
+function getStats(id){
+  //
+  let videoId = id.items
+  for(videoId of videoId){
+    var videoID = `${videoId.id.videoId}`
+    // Stats
+    fetchURL = "https://youtube.googleapis.com/youtube/v3/videos?part=statistics,snippet&id=" + videoID + "&key=AIzaSyAMMhkNlgDzRQKmBIr7NKYkxlbd52Vcv-A"
+    fetch(fetchURL).then((result)=>{
+      console.log(result)
+      return result.json()
+    }).then((videoData)=>{
+      writeHTML(videoData)
+    })
+  }
+}
+
 // Main
 
   // Get videos result
@@ -13,33 +51,9 @@ console.log("Videos.js loaded. This website uses YouTube API.")
   .then((result)=>{
     return result.json()
   }).then((id)=>{
-    // LET blank = blank
-    let videoId = id.items
-    for(videoId of videoId){
-      var videoID = `${videoId.id.videoId}`
-      // Stats
-      fetch("https://youtube.googleapis.com/youtube/v3/videos?part=statistics,snippet&id=" + videoID + "&key=AIzaSyAMMhkNlgDzRQKmBIr7NKYkxlbd52Vcv-A")
-      .then((result)=>{
-        return result.json()
-      }).then((videoData)=>{
-        let stats = videoData.items[0].statistics
-        let videos = videoData.items[0].snippet
-        let videoIndividualID = videoData.items[0].id
-        let videoContaner = document.querySelector(".videos-container")
-        // HTML
-        videoContaner.innerHTML += `
-        <section class="video">
-          <a class="videos-link" href="https://www.youtube.com/watch?v=${videoIndividualID}">
-            <img src="https://i.ytimg.com/vi/${videoIndividualID}/hq720.jpg" class="video-thumbnail" title="Thumbnail of ${videos.title}" alt="${videos.title}'s thumbnail">  
-            <h3 class="video-title" title="${videos.title}">${videos.title}</h3>
-            <div class="video-stats">
-              <img class="view-icon" src="/media/images/view-icon.svg" alt="A eye" title="views">
-              <p class="view-count">${stats.viewCount}</p>
-            </div>
-          </a>
-        </section>
-        `
-      })
-    }
+    getStats(id)
   }
 )
+
+
+
