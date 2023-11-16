2efedb69af5dc29f1ac58be8bccd3807e16b4af0
Mules Gaming <110064958+MulesGaming@users.noreply.github.com>
2023-11-16 14:44:39 -0600

    Added social media titles

--- script/all/write-dropdown.js
+++ script/all/write-dropdown.js
@@ -17,12 +17,12 @@ const dropdownMenuMainItems = `
   <!-- x -->
   <span class="dropdown-close" onclick="dropdownMenuVisible ('close')">X</span>
   <!-- Items -->
-  <a href="/"><div class="dropdown-menu-item"><img class="dropdown-menu-img" src="/media/images/home-icon.svg" width="50">Home</div></a>
-  <a href="/projects/"><div class="dropdown-menu-item"><img class="dropdown-menu-img" src="/media/images/wrench-icon.svg" width="50">Projects</div></a>
-  <a href="/yt.html"><div class="dropdown-menu-item"><img class="dropdown-menu-img" src="/media/images/yt_icon_mono_dark.png" width="50">YouTube</div></a>
-  <a href="/twt"><div class="dropdown-menu-item"><img class="dropdown-menu-img" src="/media/images/twitter_logo.png" width="50">Twitter</div></a>
-  <a href="/mastodon"><div class="dropdown-menu-item"><img class="dropdown-menu-img" src="/media/images/mastodon_logo.svg" width="50">Mastodon</div></a>
-  <a href="/github"><div class="dropdown-menu-item"><img class="dropdown-menu-img" src="/media/images/github-logo-white.svg" width="50">GitHub</div></a>
+  <a href="/"><div class="dropdown-menu-item" title="Home"><img class="dropdown-menu-img" src="/media/images/home-icon.svg" width="50">Home</div></a>
+  <a href="/projects/"><div class="dropdown-menu-item" title="Projects"><img class="dropdown-menu-img" src="/media/images/wrench-icon.svg" width="50">Projects</div></a>
+  <a href="/yt.html"><div class="dropdown-menu-item" title="YouTube - @mulesgamingmc"><img class="dropdown-menu-img" src="/media/images/yt_icon_mono_dark.png" width="50">YouTube</div></a>
+  <a href="/twt"><div class="dropdown-menu-item" title="Twitter - @mulesgaming"><img class="dropdown-menu-img" src="/media/images/twitter_logo.png" width="50">Twitter</div></a>
+  <a href="/mastodon"><div class="dropdown-menu-item" title="Mastodon - @mulesgaming@mastodon.social"><img class="dropdown-menu-img" src="/media/images/mastodon_logo.svg" width="50">Mastodon</div></a>
+  <a href="/github"><div class="dropdown-menu-item" title="Github - MulesGaming"><img class="dropdown-menu-img" src="/media/images/github-logo-white.svg" width="50">GitHub</div></a>
 </div>
 `;
 
