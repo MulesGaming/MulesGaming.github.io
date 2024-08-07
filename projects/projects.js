77e5874176e087eb78c11efeb8c66e7f4b9f7230
Mules <20223774-mulesgaming@users.noreply.gitlab.com>
2024-08-06 19:37:53 -0500

    Added brave logo

--- projects/projects.js
+++ projects/projects.js
@@ -2,7 +2,7 @@
 
 projects = [
   {name:"Minecraft Wordle",desc:"The Wordle, but Minecraft!",link:"https://minecraftwordle.fun",img:"https://minecraftwordle.fun/media/img/logo.png"},
-  {name:"Brave debullshitinator",desc:"Removes unessasary bloat from Brave Browser.",link:"https://github.com/MulesGaming/brave-debullshitinator",img:"https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fgeeksterminal.com%2Fwp-content%2Fuploads%2F2020%2F04%2Fbrave_icon.png&f=1&nofb=1&ipt=a78b26e422873a85d9fdf608ea0bfa5e49c7c6ca7df1f6b0ec7b0400018d876b&ipo=images"},
+  {name:"Brave debullshitinator",desc:"Removes unessasary bloat from Brave Browser.",link:"https://github.com/MulesGaming/brave-debullshitinator",img:"/media/images/brave-logo.png"},
   {name:"My Minecraft skins",desc:"My Minecraft skins!",link:"/projects/skins",img:""},
   {name:"My Datapacks for Minecraft",desc:"Datapacks I've made!",link:"/projects/datapacks",img:""},
   {name:"Search for videos",desc:"Search for videos across my channel!",link:"/projects/search",img:""}
